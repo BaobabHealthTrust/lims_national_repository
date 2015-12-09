07594cf5df79db4ff0e4555954619d8f0a193467
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2015-12-09 23:54:44 +0200

    Added page and logic for remote call page for creating orders

---
+++ public/javascripts/lab_order.js
@@ -0,0 +1,91 @@
+/**
+ * Created by chimwemwe on 12/8/15.
+ */
+
+// http://localhost:3014/lab_order?hide_demographics=false&first_name=Mary&last_name=Banda&gender=F&date_of_birth=1978-03-04&national_patient_id=XXYYZZ&phone_number=0999999999&reason_for_test=Routine&sample_collector_last_name=Doctor&sample_collector_first_name=Test&sample_collector_phone_number=0888888888&sample_collector_id=FVG564&district=Lilongwe&health_facility_name=Kamuzu+Central+Hospital&middle_name=Chipiliro&return_path=http://localhost/lims/lab_order.php&sample_order_location=Ward+4B
+
+"use strict"
+
+function __$(id) {
+    return document.getElementById(id);
+}
+
+function ajaxLoad(url, control) {
+
+    if (control) {
+
+        control.innerHTML = "";
+
+    }
+
+    // var url = "/sample_tests/" + id.toLowerCase().replace(/\s/g, "+");
+
+    var httpRequest;
+    if (window.XMLHttpRequest) {
+        httpRequest = new XMLHttpRequest();
+    }
+
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+
+            if (httpRequest.responseText.trim().length > 0) {
+
+                var json = JSON.parse(httpRequest.responseText);
+
+                for (var i = 0; i < json.length; i++) {
+
+                    var opt = document.createElement("option");
+
+                    opt.innerHTML = json[i];
+
+                    if (control) {
+
+                        control.appendChild(opt);
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+
+}
+
+function validateForm() {
+
+    var controls = ["district", "health_facility_name", "first_name", "last_name", "gender", "date_of_birth",
+            "national_patient_id", "reason_for_test", "sample_collector_last_name", "sample_collector_first_name",
+            "sample_collector_phone_number", "sample_collector_id", "sample_type", "date_sample_drawn", "tests",
+            "target_lab", "sample_order_location", "return_path"];
+
+    for(var i = 0; i < controls.length; i++) {
+
+        if (__$(controls[i])) {
+
+            if(__$(controls[i]).value.trim().length <= 0) {
+
+                showMsg("Please fill all required fields!");
+
+                __$(controls[i]).focus();
+
+                return;
+
+            }
+
+        }
+
+    }
+
+    document.forms[0].submit();
+
+}
+
+function showMsg(msg) {
+
+    alert(msg);
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-
 /*
  * GET home page.
  */
@@ -9,21 +8,21 @@ module.exports = function (router) {
 
     var path = require("path");
 
-    var couch = require(path.resolve('public','javascripts','couch.js'));
+    var couch = require(path.resolve('public', 'javascripts', 'couch.js'));
 
     var url = require('url');
 
-    var configs = fs.readFileSync(path.resolve('config','couchdb.json'));
+    var configs = fs.readFileSync(path.resolve('config', 'couchdb.json'));
 
     var locks = require("locks");
 
     var mutex = {};
 
-    function padZeros(number, positions){
+    function padZeros(number, positions) {
         var zeros = parseInt(positions) - String(number).length;
         var padded = "";
 
-        for(var i = 0; i < zeros; i++){
+        for (var i = 0; i < zeros; i++) {
             padded += "0";
         }
 
@@ -32,19 +31,19 @@ module.exports = function (router) {
         return padded;
     }
 
-    function doCreateRecord(json, callback){
+    function doCreateRecord(json, callback) {
 
         console.log(json);
 
         var site = JSON.parse(configs)["site_code"];
 
-        if(!site) {
+        if (!site) {
 
             return null;
 
         }
 
-        if(!mutex[site]) {
+        if (!mutex[site]) {
 
             mutex[site] = locks.createMutex();
 
@@ -55,7 +54,7 @@ module.exports = function (router) {
             err: true
         };
 
-        if(mutex[site].tryLock()) {
+        if (mutex[site].tryLock()) {
 
             console.log("locked mutex");
 
@@ -122,10 +121,10 @@ module.exports = function (router) {
 
                     var offset = (pbody[formattedDate] ? pbody[formattedDate].offset : 1);
 
-                    var id = site + date.getFullYear().toString().substring(2,4) + months[date.getMonth()] +
+                    var id = site + date.getFullYear().toString().substring(2, 4) + months[date.getMonth()] +
                         days[date.getDate()] + padZeros(offset, 3);
 
-                    if(!pbody[formattedDate]) {
+                    if (!pbody[formattedDate]) {
 
                         pbody[formattedDate] = {
                             offset: 2
@@ -159,7 +158,7 @@ module.exports = function (router) {
 
                     var offset = 1;
 
-                    var id = site + date.getFullYear().toString().substring(2,4) + months[date.getMonth()] +
+                    var id = site + date.getFullYear().toString().substring(2, 4) + months[date.getMonth()] +
                         days[date.getDate()] + padZeros(offset, 3);
 
                     var params = {
@@ -210,7 +209,7 @@ module.exports = function (router) {
 
         var site = JSON.parse(configs)["site_code"];
 
-        if(!site) {
+        if (!site) {
 
             return {};
 
@@ -220,7 +219,7 @@ module.exports = function (router) {
 
         couch.db(db, 'read', {'_id': id}, function (err, pbody) {
 
-            if(!err) {
+            if (!err) {
 
                 callback(pbody);
 
@@ -240,7 +239,7 @@ module.exports = function (router) {
 
         var site = JSON.parse(configs)["site_code"];
 
-        if(!site) {
+        if (!site) {
 
             return {};
 
@@ -250,7 +249,7 @@ module.exports = function (router) {
 
         couch.db(db, 'view', id, function (err, pbody) {
 
-            if(!err) {
+            if (!err) {
 
                 callback(pbody);
 
@@ -272,7 +271,7 @@ module.exports = function (router) {
 
         var site = JSON.parse(configs)["site_code"];
 
-        if(!site) {
+        if (!site) {
 
             return {};
 
@@ -282,7 +281,7 @@ module.exports = function (router) {
 
         couch.db(db, 'save', json, function (error, body) {
 
-            if(!error) {
+            if (!error) {
 
                 callback(true);
 
@@ -308,19 +307,19 @@ module.exports = function (router) {
 
             var params = req.body;
 
-            if(params.data) {
+            if (params.data) {
 
                 params = params.data;
 
             }
 
-            doCreateRecord(params, function(result) {
+            doCreateRecord(params, function (result) {
 
-                if(result.id == null) {
+                if (result.id == null) {
 
                     res.status(200).json({error: true, message: "Configuration file not found!", data: null});
 
-                } else if(result.err) {
+                } else if (result.err) {
 
                     res.status(200).json({error: true, message: "Process busy, please try again later!", data: null});
 
@@ -337,7 +336,7 @@ module.exports = function (router) {
     router.route('/query_order/:id')
         .get(function (req, res) {
 
-            doRead(req.params.id, function(result) {
+            doRead(req.params.id, function (result) {
 
                 res.status(200).json({data: result});
 
@@ -352,7 +351,7 @@ module.exports = function (router) {
 
             var query = url_parts.query;
 
-            doRead(query.id, function(result) {
+            doRead(query.id, function (result) {
 
                 res.status(200).json({data: result});
 
@@ -363,7 +362,7 @@ module.exports = function (router) {
     router.route('/query_order_by_npid/:id')
         .get(function (req, res) {
 
-            doReadByNPID(req.params.id, function(result) {
+            doReadByNPID(req.params.id, function (result) {
 
                 res.status(200).json({data: result});
 
@@ -376,15 +375,15 @@ module.exports = function (router) {
 
             var params = req.body;
 
-            if(params.data) {
+            if (params.data) {
 
                 params = params.data;
 
             }
 
-            doRead(params._id, function(result) {
+            doRead(params._id, function (result) {
 
-                if(Object.keys(result).length > 0) {
+                if (Object.keys(result).length > 0) {
 
                     var json = params;
 
@@ -392,9 +391,9 @@ module.exports = function (router) {
 
                     json._rev = result._rev;
 
-                    doUpdate(json, function(success) {
+                    doUpdate(json, function (success) {
 
-                        if(success) {
+                        if (success) {
 
                             res.status(200).json({status: "SUCCESS", data: json});
 
@@ -426,14 +425,28 @@ module.exports = function (router) {
             var sample_types = ['Dry Blood Spot', 'Whole Blood', 'Sputum', 'Pus'];
 
             res.render('lab_order', {
-                first_name:             (query.first_name || ""),
-                last_name:              (query.last_name || ""),
-                middle_name:            (query.middle_name || ""),
-                gender:                 (query.gender || ""),
-                date_of_birth:          (query.date_of_birth || ""),
-                national_patient_id:    (query.national_patient_id || ""),
-                return_path:            (query.return_path || ""),
-                sample_types:           sample_types
+                first_name: (query.first_name || ""),
+                last_name: (query.last_name || ""),
+                middle_name: (query.middle_name || ""),
+                gender: (query.gender || ""),
+                date_of_birth: (query.date_of_birth || ""),
+                national_patient_id: (query.national_patient_id || ""),
+                return_path: (query.return_path || ""),
+                sample_types: sample_types,
+                district: (query.district || ""),
+                health_facility_name: (query.health_facility_name || ""),
+                date_sample_drawn: (query.date_sample_drawn || ""),
+                phone_number: (query.phone_number || ""),
+                reason_for_test: (query.reason_for_test || ""),
+                sample_collector_last_name: (query.sample_collector_last_name || ""),
+                sample_collector_first_name: (query.sample_collector_first_name || ""),
+                sample_collector_phone_number: (query.sample_collector_phone_number || ""),
+                sample_collector_id: (query.sample_collector_id || ""),
+                sample_order_location: (query.sample_order_location || ""),
+                sample_priority: (query.sample_priority || ""),
+                sample_type: (query.sample_type || ""),
+                hide_demographics: (query.hide_demographics.toString().toLowerCase() == "true" ? true : false),
+                info: (query.return_path ? "" : "Missing return path. Can't proceed!")
             });
 
         })
@@ -445,14 +458,206 @@ module.exports = function (router) {
                 "whole blood": ['FBC', 'WBC', 'VL', 'RBC']
             };
 
-            res.status(200).json(sample_types[req.params.id.replace(/\+/g," ").toLowerCase()]);
+            res.status(200).json(sample_types[req.params.id.replace(/\+/g, " ").toLowerCase()]);
 
         })
+
+
+    router.route('/create_hl7_order')
+        .post(function (req, res) {
+
+            var params = req.body;
+
+            if (params.data) {
+
+                params = params.data;
+
+            }
+
+            console.log(params);
+
+            var template = "MSH|^~&||^^||^^|||OML^O21^OML_O21||T|2.5\r" +
+                "PID|1||~^^^^^^||^^|||\r" +
+                "ORC||||||||||^^^|||^^^^^^^^||||||||^^^|\r" +
+                "TQ1|1||||||||^^^\r" +
+                "SPM|1|||^\r"; /*+
+                "OBR|1|||^^||||||||||||^^\r" +
+                "NTE|1|P|\r";*/
+
+            var hl7e = require("hl7");
+
+            var hl7 = hl7e.parseString(template);
+
+            var date = (new Date());
+
+            hl7[0][4][0][0] = (params.health_facility_name || "");
+
+            hl7[0][6][0][0] = (params.target_lab || "");
+
+            hl7[0][7][0][0] = date.getFullYear() + (padZeros(date.getMonth() + 1, 2)) + padZeros(date.getDate(), 2) +
+                padZeros(date.getHours()) + padZeros(date.getMinutes(), 2) + padZeros(date.getSeconds(), 2);
+
+            hl7[0][10][0][0] = date.getFullYear() + (padZeros(date.getMonth() + 1, 2)) + padZeros(date.getDate(), 2) +
+                padZeros(date.getHours()) + padZeros(date.getMinutes(), 2) + padZeros(date.getSeconds(), 2);
+
+            // hl7[1][3][0][0] = (params.national_patient_id || "");
+
+            hl7[1][5][0][0] = (params.last_name || "");
+
+            hl7[1][5][0][1] = (params.first_name || "");
+
+            hl7[1][5][0][2] = (params.middle_name || "");
+
+            if(params.date_of_birth) {
+
+                var dob = (new Date(params.date_of_birth));
+
+                var formattedDob = dob.getFullYear() + (padZeros(dob.getMonth() + 1, 2)) + padZeros(dob.getDate(), 2) +
+                    padZeros(dob.getHours()) + padZeros(dob.getMinutes(), 2) + padZeros(dob.getSeconds(), 2);
+
+                hl7[1][7][0][0] = (formattedDob || "");
+
+            }
+
+            hl7[1][8][0][0] = (params.gender || "");
+
+            hl7[1][3][1][0] = (params.national_patient_id || "");
+
+            hl7[4][4][0][1] = (params.tests || "");
+
+            hl7[2][21][0][0] = (params.health_facility_name || "");
+
+            hl7[2][10][0][0] = (params.sample_collector_id || "");
+
+            hl7[2][10][0][1] = (params.sample_collector_last_name || "");
+
+            hl7[2][10][0][2] = (params.sample_collector_first_name || "");
+
+            hl7[2][10][0][3] = (params.sample_collector_phone_number || "");
+
+            hl7[3][9][0][1] = (params.sample_priority || "");
+
+            hl7[4][4][0][1] = (params.sample_type || "");
+
+            for(var i = 0; i < params.tests.length; i++) {
+
+                hl7.push([
+                        'OBR',
+                        [ [ (i + 1).toString() ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '',
+                            '',
+                            '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '',
+                            '',
+                            '',
+                            ''] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ '' ] ],
+                        [ [ 'F' ] ],
+                        [ [ ' ' ] ] ]);
+
+                hl7.push([ 'NTE',
+                    [ [ (i + 1).toString() ] ],
+                    [ [ 'P' ] ],
+                    [ [ '' ] ] ]);
+
+                hl7[5 + (2 * i)][4][0][1] = (params.tests[i] || "");
+
+                var today = (new Date());
+
+                var dateDrawn = ((new Date(params.date_sample_drawn)) || (new Date()));
+
+                var dateDrawnFormatted = dateDrawn.getFullYear() + (padZeros(dateDrawn.getMonth() + 1, 2)) + padZeros(dateDrawn.getDate(), 2) +
+                    padZeros(today.getHours()) + padZeros(today.getMinutes(), 2) + padZeros(today.getSeconds(), 2);
+
+                hl7[5 + (2 * i)][7][0][0] = (dateDrawnFormatted || "");
+
+                hl7[5 + (2 * i)][13][0][0] = (params.reason_for_test || "");
+
+                hl7[5 + (2 * i)][16][0][0] = (params.sample_collector_id || "");
+
+                hl7[5 + (2 * i)][16][0][1] = (params.sample_collector_last_name || "");
+
+                hl7[5 + (2 * i)][16][0][2] = (params.sample_collector_first_name || "");
+
+                hl7[5 + (2 * i)][16][0][3] = (params.sample_collector_phone_number || "");
+
+                hl7[6 + (2 * i)][3][0][0] = "Drawn";
+
+            }
+
+            var hl7Str = hl7e.serializeJSON(hl7);
+
+            console.log(hl7Str.replace(/\r/g, "\n"));
+
+            var mirth = JSON.parse(configs);
+
+            var options_auth = {user: mirth.mirth_username, password: mirth.mirth_password};
+
+            var Client = require('node-rest-client').Client;
+
+            var args = {
+                data: hl7Str,
+                headers: {"Content-Type": "text/plain"}
+            };
+
+            (new Client()).put(mirth.mirth_host, args, function (data, response) {
+
+                var output = data.toString();
+
+                var resultHL7 = hl7e.parseString(output);
+
+                console.log(hl7e.serializeJSON(resultHL7).replace(/\r/g,'\n'));
+
+                var accession_number = resultHL7[5][2][0][0];
+
+                params.accession_number = accession_number;
+
+                var link = params.return_path + "?";
+
+                var keys = Object.keys(params);
+
+                for(var i = 0; i < keys.length; i++) {
+
+                    var key = keys[i];
+
+                    if(key == "return_path") continue;
+
+                    link += (link.trim().match(/\?$/) ? "" : "&") + key + "=" + encodeURI(params[key]);
+
+                }
+
+                console.log(link);
+
+                res.redirect(link);
+
+            });
+
+        });
 
     return router;
 
 }
 
 /*exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
-};*/
+ res.render('index', { title: 'Express' })
+ };*/
